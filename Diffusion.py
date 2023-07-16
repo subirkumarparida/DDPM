742e8362a13519ad7485bfd5635f477d56e25678
subirkumarparida <subirkumarparida@gmail.com>
2023-07-17 03:43:07 +0530

    Update Diffusion.py

--- Diffusion.py
+++ Diffusion.py
@@ -189,7 +189,7 @@ class UNet(nn.Module):
         inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2, device=self.device).float() / channels))
         pos_enc_a = torch.sin(t.repeat(1, channels // 2) * inv_freq)
         pos_enc_b = torch.cos(t.repeat(1, channels // 2) * inv_freq)
-        pos_enc = torch.cat([pos_enc_a ,pos_enc_b], dim=-1)
+        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
         return pos_enc
 
     def forward(self, x, t):
@@ -335,7 +335,7 @@ class UNet_conditional(nn.Module):
         inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2, device=self.device).float() / channels))
         pos_enc_a = torch.sin(t.repeat(1, channels // 2) * inv_freq)
         pos_enc_b = torch.cos(t.repeat(1, channels // 2) * inv_freq)
-        pos_enc = torch.cat([pos_enc_a ,pos_enc_b], dim=-1)
+        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
         return pos_enc
 
     def forward(self, x, t, y):
